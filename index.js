f455005490213dea7ee8ead93bb3daab06ab347c
Eduarda <eduanginho@gmail.com>
2024-12-11 19:09:21 +0100

    implemented theme toggle button, awaiting for styling

--- index.js
+++ index.js
@@ -42,6 +42,30 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //Dark-Light theme toggle
 
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("theme-toggle");
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    document.body.classList.remove("light-theme");
+  } else if (savedTheme === "light") {
+    document.body.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
+  }
+  toggleButton.addEventListener("click", function () {
+    if (document.body.classList.contains("dark-theme")) {
+      document.body.classList.remove("dark-theme");
+      document.body.classList.add("light-theme");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.body.classList.remove("light-theme");
+      document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+});
+
 //Full section scrolling
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("section");
